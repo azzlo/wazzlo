a91a936d4806c039ac9761338689a7f977d6011d
Oscar Tellez <oscartzgz@gmail.com>
2018-09-27 11:55:53 -0500

    Permitir metodo PUT sin token de verificacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # skip_before_action :verify_authenticity_token, if: :json_request?if: :json_request?
+  skip_before_action :verify_authenticity_token, only: [:update]
+
+  def json_request?
+    request.format.json?
+  end
 end
