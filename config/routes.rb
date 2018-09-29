f19c3b73d5b0dff9dc5e8cb8d3d5ba733b32c811
Oscar Tellez <oscartzgz@gmail.com>
2018-09-28 22:20:46 -0500

    Ruta anidada para Contenedores

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :containers
+  resources :containers do
+    # resources :container_histories, only: [:index]
+    collection do
+      get 'history', to: 'container_histories#index'
+    end
+  end
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
